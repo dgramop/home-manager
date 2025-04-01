5eb9899ad656fcb860fa965d8cedf228ac55674f
Dhruv Gramopadhye <dgramopadhye@anduril.com>
2025-04-01 12:32:39 -0700

    subst

--- home.nix
+++ home.nix
@@ -1,23 +1,35 @@
-{pkgs, lib, config, ...}: 
-let 
-  lattice = (builtins.getFlake "git+ssh://git@ghe.anduril.dev/anduril/latticectl?rev=292966803a03b4a21ee1ea6538e8696287e8b6cc").packages.x86_64-linux.default;
-in {
+{pkgs, lib, config, ...}: {
   imports = [
     ./common.nix
   ];
 
   config = {
-    home.username = "dgramopadhye";
-    home.homeDirectory = "/home/dgramopadhye";
+    home.username = "dgramop";
+    home.homeDirectory = "/Users/dgramop";
     common = {
       enable = true;
-      email = "dgramopadhye@anduril.com";
+      email = "dgramopadhye@gmail.com";
       name = "Dhruv Gramopadhye";
     };
-    
-    home.shellAliases.rf = "nix develop .#nightly --command bash -c \"cargo fmt\"";
-    home.packages = [
-      lattice
+
+    home.packages = with pkgs; [
+      darwin.lsusb
+      gnuradio
     ];
+
+    programs.git.extraConfig = {
+      gpg = {
+        format = "ssh";
+      };
+      "gpg \"ssh\"" = {
+        program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+      };
+      commit = {
+        gpgsign = true;
+      };
+      user = {
+        signingKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCljmP0GXaGu97J/vOE5kvlKZt262sqx2ADiN0Glt5dMjiP4ubQLmC4vUr4rajV/n1JcTJzp12LSNmIVUQKLZgxLpwKhk7W7EAElT2rCMj6Yr1c2P5B34nGCyDYPjMWahupkZafLHze9zWxtkH+fHicH4GtOXMW4R9nZycwqtefAUsWBSbG023rYgzO9lUz8ZPb846CgwxWdtDoOdf15O58IrRfrWF3QKzWErli3OZ5K4cu70D55xCyGG9+Gpozf1u0kTF80jCb24TNr2CELEo8rqVXmJeVqA5LO1g5putLzzeTt8XL6tBjT2Wu0eQAAVOODee51QXCQ8dM29HaT7rbodeWEBrfAIY0V8FsjGQSpQv0VmcDzTyQH7Se29Pd6kPYP8M3VjPoTK+RMHSOdgTPY7iAgUo5c5qhs4DA3vXI+CgaEopL3AiKOtycYOhkMB/HGcQZiZ126BCRlr7exeM7d5/XQsNjhuLjyAnOxsWNA8DI0IvmRflakka2gVqEYRk=";
+      };
+    };
   };
 }
