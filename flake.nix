a288ed8125f2654f0c2b6496ea34330792a836ba
Dhruv Gramopadhye <dgramopadhye@gmail.com>
2025-06-04 01:37:19 -0700

    cleanup

--- flake.nix
+++ flake.nix
@@ -1,23 +1,26 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
-    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = {self, nixpkgs, flake-utils, ...}: flake-utils.lib.eachDefaultSystem (system: let
+  outputs = {self, nixpkgs, flake-utils, home-manager, ...}: flake-utils.lib.eachDefaultSystem (system: let
     fixSdcc = final: prev: {
       # Temporary fix until 413537 merges
       sdcc = prev.sdcc.overrideAttrs {
         outputs = ["out" "doc"];
       };
     };
-
-    pkgs = (import nixpkgs {
-      overlays = [fixSdcc];
-    }).legacyPackages.${system};
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
   in {
-    homeConfigurations.dgramop = pkgs.callPackage ./home.nix {config = {};};
+    packages.homeConfigurations."dgramop" = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+      modules = [ ./home.nix ];
+    };
   });
 }
