ac1ecc0f65e0211f587f0acd3b2abf36d3a078f5
Dhruv Gramopadhye <dgramopadhye@anduril.com>
2025-06-17 16:25:34 -0700

    changed files

--- common.nix
+++ common.nix
@@ -40,7 +40,7 @@
       delta
       drawio
     ];
-    home.stateVersion = "24.11";
+    home.stateVersion = "25.05";
 
     # Home Manager is pretty good at managing dotfiles. The primary way to manage
     # plain files is through 'home.file'.
